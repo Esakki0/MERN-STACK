743a8b84661b911966b1de1d44f330ecc3404cb0
Esakki0 <22ad016@psr.edu.in>
2025-03-06 11:39:24 +0530

    Delete backend directory

--- backend/config/db.js
+++
@@ -1,5 +0,0 @@
-import mongoose from "mongoose";
-
-export const connectDB = async () => {
-    (await mongoose.connect('mongodb+srv://root:root@cluster0.yu7ad.mongodb.net/online?retryWrites=true&w=majority&appName=Cluster0')).isObjectIdOrHexString(()=>console.log("DB Connected"));
-}
